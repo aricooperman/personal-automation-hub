79317a56851c7299286c6e977b972c1c59927fb6
Ari Cooperman <aricooperman@gmail.com>
2021-11-18 20:27:35 -0700

    Cleaned up for sharing

--- constants.py
+++ constants.py
@@ -1,7 +1,8 @@
 import pytz
+from configuration import configs
 
 PNG_MIME_TYPE = 'image/png'
 PDF_MIME_TYPE = 'application/pdf'
 
-DEFAULT_TZ = 'America/Denver'
+DEFAULT_TZ = configs['timezone']
 LOCAL_TZ = pytz.timezone(DEFAULT_TZ)
