f229a03bb1d6a5fc9b795d1cc8be194ed7ab45fc
Ari Cooperman <aricooperman@gmail.com>
2023-07-18 20:27:29 -0600

    If pdftotext doesn't find text then extract images and run ocr on those

--- utils/ocr.py
+++ utils/ocr.py
@@ -9,12 +9,17 @@ def get_image_full_text(img_file_like: IO) -> str:
         with open(tmp_file_name, mode="wb") as img_file:
             img_file.write(img_file_like.read())
 
-        ocr_cmd = f"tesseract -l eng '{tmp_file_name}' -"
-        out_pipe = os.popen(ocr_cmd, mode="r")
-        img_text = ""
-        lines = out_pipe.readlines()
-        for line in lines:
-            img_text += '> ' + line.replace('>', r'\>')
+        img_text = run_image_ocr(tmp_file_name)
 
         os.remove(tmp_file_name)
         return img_text
+
+
+def run_image_ocr(file_name: str):
+    ocr_cmd = f"tesseract -l eng '{file_name}' -"
+    out_pipe = os.popen(ocr_cmd, mode="r")
+    img_text = ""
+    lines = out_pipe.readlines()
+    for line in lines:
+        img_text += '> ' + line.replace('>', r'\>')
+    return img_text
--- utils/pdf.py
+++ utils/pdf.py
@@ -1,6 +1,9 @@
 import os
+import glob
 import tempfile
 from typing import IO
+
+from utils.ocr import run_image_ocr
 
 
 def get_pdf_full_text(pdf_file_like: IO) -> str:
@@ -10,10 +13,23 @@ def get_pdf_full_text(pdf_file_like: IO) -> str:
             pdf_file.write(pdf_file_like.read())
 
         pdf_to_text_cmd = f"pdftotext -nopgbrk -layout '{tmp_file_name}' -"
-        out_pipe = os.popen(pdf_to_text_cmd, mode="r")
         pdf_text = ""
+        out_pipe = os.popen(pdf_to_text_cmd, mode="r")
         lines = out_pipe.readlines()
         for line in lines:
             pdf_text += '> ' + line.replace('>', r'\>')
+
+        if len(pdf_text.strip()) == 0:
+            pdfimage_cmd = f"pdfimages -tiff '{tmp_file_name}' '{tmpdir}/image'"
+            if os.system(pdfimage_cmd) != 0:
+                print("Failed to run pdfimage command")
+                return ""
+
+            for file in sorted(glob.glob(f"{tmpdir}/image-*")):
+                image_txt = run_image_ocr(file)
+                if len(image_txt) > 0:
+                    pdf_text += image_txt
+
         os.remove(tmp_file_name)
+
         return pdf_text
